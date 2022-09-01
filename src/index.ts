2dec2e142cd60334ed3a7b66caeefc23d80701f4
Shu Ding <g@shud.in>
2022-09-01 17:02:19 +0200

    configurable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import { showHUD, Clipboard } from "@raycast/api";
+import { showHUD, Clipboard, getPreferenceValues } from "@raycast/api";
+
+interface Preferences {
+  "branch-prefix"?: string;
+  "suffix-length"?: string;
+}
 
 export default async function main() {
-  const id = (Math.random() * 10000000).toString(36).substring(0, 4);
-  await Clipboard.copy(`shu/${id}`);
+  const preferences = getPreferenceValues<Preferences>();
+
+  const prefix = preferences["branch-prefix"];
+  const length = parseInt(preferences["suffix-length"] || "4", 10);
+  const id = (Math.random() * 1e20).toString(36).substring(0, length);
+
+  await Clipboard.copy((prefix ? prefix + "/" : "") + id);
   await showHUD("Copied branch name to clipboard");
 }
