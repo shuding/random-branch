5069cc5e0c92a0610342fce92575225495309521
devjiwonchoi <devjiwonchoi@gmail.com>
2024-08-23 01:45:16 +0900

    feat: add an option to include `git checkout -b` when copying

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import { showHUD, Clipboard, getPreferenceValues } from "@raycast/api";
 interface Preferences {
   "branch-prefix"?: string;
   "suffix-length"?: string;
+  "include-git-checkout-b"?: boolean;
 }
 
 export default async function main() {
   const preferences = getPreferenceValues<Preferences>();
 
-  const prefix = preferences["branch-prefix"];
+  const prefix = preferences["branch-prefix"] ? preferences["branch-prefix"] + "/" : "";
   const length = parseInt(preferences["suffix-length"] || "4", 10);
+  const gitCheckoutB = preferences["include-git-checkout-b"] ? "git checkout -b " : "";
   const id = (Math.random() * 1e20).toString(36).substring(0, length);
 
-  await Clipboard.copy((prefix ? prefix + "/" : "") + id);
-  await showHUD("Copied branch name to clipboard");
+  const result = gitCheckoutB + prefix + id;
+  await Clipboard.copy(result);
+  await showHUD(`Copied to your clipboard: ${result}`);
 }
